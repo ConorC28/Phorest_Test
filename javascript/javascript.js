c8a1fa60eaf53a91fe274cba7ff7d1513a93423e
Conor Conlon <conorconlon@gmail.com>
2019-10-06 14:35:15 +0100

    GETting data to HTML

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,39 +1,55 @@
-var Username = "global/cloud@apiexamples.com";
-var Password =  "VMlRo/eh+Xd8M~l";
-var Endpoint = "https//api-gateway-dev.phorest.com/third-party-api-server/api/business/";
+var userName = "global/cloud@apiexamples.com";
+var passWord = "VMlRo/eh+Xd8M~l";
 
-function searchClients(Username = 'global/cloud@apiexamples.com', Password =  'VMlRo/eh+Xd8M~l' ){
-  var Username = "global/cloud@apiexamples.com";
-  var Password =  "VMlRo/eh+Xd8M~l";
-  var Endpoint = "https//api-gateway-dev.phorest.com/third-party-api-server/api/business/";
+function authenticateUser(user, password)
+{
+    var token = user + ":" + password;
 
+    // Should i be encoding this value????? does it matter???
+    // Base64 Encoding -> btoa
+    var hash = btoa(token);
 
-
-  var x = document.getElementById("data");
-  if (x.style.display != "block") {
-    x.style.backgroundColor = "red";
-  } else {
-    x.style.backgroundColor = "green";
-  }
-  fetch('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clientcourse/')
-  .then(response => response.json())
-  .then(json => display(json))
+    return "Basic " + hash;
 }
 
 
+function searchClients(json){
 
-function getData(){
-  var x = document.getElementById("data");
-  if (x.style.display != "block") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
 
-  fetch('https://jsonplaceholder.typicode.com/todos/')
-  .then(response => response.json())
-  .then(json => display(json))
+
+
+
+  var request_ = new XMLHttpRequest();
+
+      request_.open("GET", "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client/", true);
+      request_.setRequestHeader("Authorization", authenticateUser(userName, passWord));
+      request_.send();
+
+
+
+      request_.onreadystatechange = function () {
+
+          if (request_.readyState == 4 && request_.status == 200) {
+
+
+            var data = JSON.parse(request_.responseText);
+            var response = request_.responseText;
+            var obj = JSON.parse(response);
+            var str = JSON.stringify(obj._embedded.clients, null, 2);
+
+            console.log(obj._embedded.clients)
+
+    document.getElementById("searchData").innerHTML = str;
+
+              // handle data as needed...
+
+          }
+      }
+
+
 }
+
+
 
 function display(json) {
   let myData = ''
@@ -46,5 +62,70 @@ function display(json) {
     value.completed+'</div>'+
     '</div><br>'
  })
-    document.getElementById("data").innerHTML= myData
+    document.getElementById("searchData").innerHTML= myData
 }
+
+
+
+
+
+    // request.open("GET", "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clientcourse/", false);
+    // request.setRequestHeader("Authorization", authenticateUser(userName, passWord));
+    // request.send();
+    // fetch('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clientcourse/')
+    // .then(response => response.json())
+    // .then(json => display(json))
+    // view request status
+    //alert(request.status);
+    //response.innerHTML = request.responseText;
+
+
+
+
+
+
+
+
+
+// function searchClients(){
+// var x = document.getElementById("searchData");
+// fetch('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client/')
+// .then(response => response.json())
+// .then(json => display(json))
+// }
+//
+// function display(json) {
+//   let myData = ''
+//    $.each(json, function(key,value) {
+//     myData += '<div class="square"> ' +
+//     '<div class="circle">' +
+//     value.id+'</div><div>'+
+//     value.userId+ '</div><div>'+
+//     value.title+'</div><div>'+
+//     value.completed+'</div>'+
+//     '</div><br>'
+//  })
+//     document.getElementById("searchData").innerHTML= myData
+// }
+
+
+// javascript
+function getData(){
+fetch('https://jsonplaceholder.typicode.com/todos/')
+.then(response => response.json())
+.then(json => display(json))
+}
+
+// function display(json) {
+//   let myData = ''
+//    $.each(json, function(key,value) {
+//     myData += '<div class="square"> ' +
+//     '<div class="circle">' +
+//     value.id+'</div><div>'+
+//     value.userId+ '</div><div>'+
+//     value.title+'</div><div>'+
+//     value.completed+'</div>'+
+//     '</div><br>'
+//  })
+//     document.getElementById("data").innerHTML= myData
+// }

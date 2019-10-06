61e5443e0c24941e60a3307e3ee84a5b5d2c1cbc
Conor Conlon <conorconlon@gmail.com>
2019-10-06 21:32:31 +0100

    final commit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,21 @@
+// authentication values
+
 var userName = "global/cloud@apiexamples.com";
 var passWord = "VMlRo/eh+Xd8M~l";
 
-function authenticateUser(user, password)
-{
-    var token = user + ":" + password;
 
-    // Should i be encoding this value????? does it matter???
-    // Base64 Encoding -> btoa
+//save token
+
+function authenticateUser(username, password)
+{
+    var token = username + ":" + password;
+
     var hash = btoa(token);
 
     return "Basic " + hash;
 }
 
-
-function searchClients(json){
-
-
-
-
+function searchClients(){
 
   var request_ = new XMLHttpRequest();
 
@@ -25,107 +23,32 @@ function searchClients(json){
       request_.setRequestHeader("Authorization", authenticateUser(userName, passWord));
       request_.send();
 
-
-
       request_.onreadystatechange = function () {
 
           if (request_.readyState == 4 && request_.status == 200) {
 
 
-            var data = JSON.parse(request_.responseText);
+// save user input to compare with json object values
+            var searchContent = document.getElementById('searchContent').value;
+
             var response = request_.responseText;
             var obj = JSON.parse(response);
             var str = JSON.stringify(obj._embedded.clients, null, 2);
+            var cliId = JSON.stringify(obj._embedded.clients.clientId, null, 2);
 
-            console.log(obj._embedded.clients)
+            var client1id = JSON.stringify(obj._embedded.clients[0].clientId);
+            var jClients = JSON.parse(str);
 
-    document.getElementById("searchData").innerHTML = str;
+            
+            document.getElementById("searchCount").innerHTML = obj._embedded.clients.length;
+            document.getElementById("searchData").innerHTML = str;
+//alert(obj._embedded.clients[0].clientId);
 
-              // handle data as needed...
 
+
+console.log(client1id);
+
+
+                   }
+            }
           }
-      }
-
-
-}
-
-
-
-function display(json) {
-  let myData = ''
-   $.each(json, function(key,value) {
-    myData += '<div class="square"> ' +
-    '<div class="circle">' +
-    value.id+'</div><div>'+
-    value.userId+ '</div><div>'+
-    value.title+'</div><div>'+
-    value.completed+'</div>'+
-    '</div><br>'
- })
-    document.getElementById("searchData").innerHTML= myData
-}
-
-
-
-
-
-    // request.open("GET", "https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clientcourse/", false);
-    // request.setRequestHeader("Authorization", authenticateUser(userName, passWord));
-    // request.send();
-    // fetch('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/clientcourse/')
-    // .then(response => response.json())
-    // .then(json => display(json))
-    // view request status
-    //alert(request.status);
-    //response.innerHTML = request.responseText;
-
-
-
-
-
-
-
-
-
-// function searchClients(){
-// var x = document.getElementById("searchData");
-// fetch('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client/')
-// .then(response => response.json())
-// .then(json => display(json))
-// }
-//
-// function display(json) {
-//   let myData = ''
-//    $.each(json, function(key,value) {
-//     myData += '<div class="square"> ' +
-//     '<div class="circle">' +
-//     value.id+'</div><div>'+
-//     value.userId+ '</div><div>'+
-//     value.title+'</div><div>'+
-//     value.completed+'</div>'+
-//     '</div><br>'
-//  })
-//     document.getElementById("searchData").innerHTML= myData
-// }
-
-
-// javascript
-function getData(){
-fetch('https://jsonplaceholder.typicode.com/todos/')
-.then(response => response.json())
-.then(json => display(json))
-}
-
-// function display(json) {
-//   let myData = ''
-//    $.each(json, function(key,value) {
-//     myData += '<div class="square"> ' +
-//     '<div class="circle">' +
-//     value.id+'</div><div>'+
-//     value.userId+ '</div><div>'+
-//     value.title+'</div><div>'+
-//     value.completed+'</div>'+
-//     '</div><br>'
-//  })
-//     document.getElementById("data").innerHTML= myData
-// }
